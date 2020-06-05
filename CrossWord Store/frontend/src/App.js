dcb513e58a8de3df6047426a0b2a08f441723ca3
GuiestresanT <estreano.guilherme@outlook.com>
2020-06-05 11:03:38 -0300

    Projeto
    Tentativa de subir o projeto pro GitHub

---
+++ CrossWord Store/frontend/src/App.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter, Route, Link} from 'react-router-dom'
+import data from './data';
+import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
+
+function App() {
+
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open");
+  }
+
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open");
+  }
+
+  return (
+    <BrowserRouter>
+    <div className="grid-container">
+            <header className="header">
+                <div className="brand">
+                    <button onClick={openMenu}>
+                        &#9776;
+                    </button>
+                    <Link to='/'>CrossWord Store</Link>
+                  </div>
+                <div className="header-links">
+                    <a href="cart.html">Carrinho</a> 
+                    <a href="signin">Cadastre-se</a> 
+                </div>
+                
+            </header>
+            <aside className="sidebar">
+                <h3>Shopping Categories</h3>
+                <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+                <ul>
+                    <li>
+                        <a href="index.html">Livro 1</a>
+                    </li>
+                    <li>
+                        <a href="index.html">Livro 2</a>
+                    </li>
+                </ul>
+
+            </aside>
+            <main className="main">
+                <div className="content">
+                  <Route path="/products/:id" component={ProductScreen} />
+                  <Route path="/" exact={true} component={HomeScreen} />
+                  
+                </div>
+            </main>
+            <footer className="footer"> 
+               
+               Trabalho de Sistemas Distribuidos
+               
+            </footer>    
+        </div>
+     </BrowserRouter>   
+  );
+}
+
+export default App;
