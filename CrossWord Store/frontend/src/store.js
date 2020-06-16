dca502f7228f0dc25591974eeabc424fac134add
GuiestresanT <estreano.guilherme@outlook.com>
2020-06-16 12:43:57 -0300

    commit on 'master'

--- CrossWord Store/frontend/src/store.js
+++ CrossWord Store/frontend/src/store.js
@@ -1,11 +1,17 @@
 import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
+import Cookies from 'js-cookie';
 import { productListReducer, productDetailsReducer } from './reducers/productReducers';
+import {cartReducer} from './reducers/cartReducers';
 
-const initialState = {};
+const cartItems = Cookies.getJSON("cartItems") || [];
+
+
+const initialState = { cart: {cartItems}};
 const reducer = combineReducers({
     productList: productListReducer,
-    productDetails: productDetailsReducer
+    productDetails: productDetailsReducer,
+    cart: cartReducer,
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
